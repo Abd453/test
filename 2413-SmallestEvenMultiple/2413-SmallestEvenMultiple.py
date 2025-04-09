f035146237de53208832e2be7a6cb3abb344dc2e
Abd453 <134734720+Abd453@users.noreply.github.com>
2025-04-09 21:32:26 +0300

    Create file

---
+++ 2413-SmallestEvenMultiple/2413-SmallestEvenMultiple.py
@@ -0,0 +1,14 @@
+# Last updated: 09/04/2025, 18:32:26
+from typing import List
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 == 0:
+            return n
+        elif n % 2 != 0:
+            return n*2
+       
+
+
+
+ 
